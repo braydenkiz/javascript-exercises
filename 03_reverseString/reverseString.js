f0b20426fd26ce93bcaa4ac658a308cea7f33bbc
brayden kiz <115608437+braydenkiz@users.noreply.github.com>
2024-05-11 16:11:18 -0500

    created a for loop that counts down to reverse

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
+const reverseString = function(str) {
     let result = "";
-    
+    for(let i = str.length - 1; i >= 0;i--){
+
+    }
+
 };
 
 // Do not edit below this line
