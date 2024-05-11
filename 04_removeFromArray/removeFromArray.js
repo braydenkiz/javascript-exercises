2bd2e60f0628d95dfb938a7a5253a384d7403fc7
brayden kiz <115608437+braydenkiz@users.noreply.github.com>
2024-05-11 18:38:55 -0500

    finished excercise partially

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr,...num) {
+    const result = [];
+    arr.forEach(element => {
+        if(!num.includes(element)){
+            result.push(element);
+        }
+    });
+    
+    return result;
 };
 
 // Do not edit below this line
